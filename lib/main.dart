62f4f50f7163e1df606d2e7be14052a7ec6c73ad
Sumi4482 <sbanwakde4482@gmail.com>
2022-07-20 10:37:23 +0530

    Added Workers Code

--- lib/main.dart
+++ lib/main.dart
@@ -11,42 +11,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: "Unique ID ",
+      title: " Workers ",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Unique ID"),
+          title: Text("Workers"),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              GetBuilder<MyController>(
-                id: 'txtCount',
-                builder: (controller) {
-                  return Text(
-                    "The value is ${controller.count}",
-                    style: TextStyle(fontSize: 25),
-                  );
-                },
-              ),
-              GetBuilder<MyController>(
-                builder: (controller) {
-                  return Text(
-                    "The value is ${controller.count}",
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
-                  );
-                },
-              ),
-              SizedBox(
-                height: 10,
-              ),
               ElevatedButton(
                 onPressed: () => myController.increment(),
                 child: Text("Increment"),
-              )
+              ),
+              Padding(
+                padding: EdgeInsets.all(16),
+                child: TextField(
+                  onChanged: ((value) {
+                    myController.increment();
+                  }),
+                ),
+              ),
             ],
           ),
         ),
--- lib/my_controller.dart
+++ lib/my_controller.dart
@@ -1,10 +1,38 @@
 import 'package:get/get.dart';
 
 class MyController extends GetxController {
-  int count = 10;
+  var count = 0.obs;
   void increment() {
-    print("HELLO");
-    ++count;
-    update(['txtCount']);
+    count++;
+  }
+
+  @override
+  void onInit() {
+    //called every time when the value of count variable changes
+    ever(count, (_) => print(count));
+
+    //called every time when the value of any variable from the list changes
+    //everAll(([count]), (_) => print(count));
+
+    //called only once when the variable value changes
+    // once(count, (_) => print(count));
+
+    //called every time the user stops typing for the 1 second
+    // debounce(
+    //     count,
+    //     (_) =>
+    //         print("when the user stop typing for 1 sec the value of counter"),
+    //     time: Duration(seconds: 1));
+
+    //Ignore all changes within 3 second.
+    //Imagine that user can earn coins by clicking on something
+    // if he clicke 300 times in the same minute
+    //he would have 300 coins , using interval
+    // you can set a time frame for 3 seconds
+    // and even then cliking 300 or a thousanf times,
+    // the maximum he would get in 1 minutes would be 20 coins
+    //clicking 300 or 1  million times
+    //interval (count,(_)=>print("Ignore all changes"),time:Duration(seconds:3));
+    super.onInit();
   }
 }
